7e5a905fd96ed7a372c7857861833630e5293acd
mintdk <yaritzacarmenluna89@gmail.com>
2025-12-27 22:13:54 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function selectLanguage(lang) {
+  document.getElementById("options").classList.remove("hidden");
+}
+
+function selectMode(mode) {
+  document.getElementById("styles").classList.remove("hidden");
+}
